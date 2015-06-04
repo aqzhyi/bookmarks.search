a1a8a2150911ac34c9f565299bdd2c8b4d956075
pleasurazy <pleasurazy@gmail.com>
2015-06-04 20:39:16 +0800

    fixed unexpected query are matched

--- src/bg/lib/extractQuery.js
+++ src/bg/lib/extractQuery.js
@@ -6,7 +6,7 @@ function extractQuery(url) {
 
   if (typeof url !== 'string') return false;
 
-  let regexp = /(?:q|p|query|search|search_query)[=\/](.*?)(?:[&\/].*?)?$/i;
+  let regexp = /[#\?=&\/]+(?:q|p|query|search|search_query)[=\/](.*?)(?:[&\/].*?)?$/i;
 
   let result = url.match(regexp);
 
--- test/query.js
+++ test/query.js
@@ -6,11 +6,17 @@ var extractQuery = require('../dist/bg/lib/extractQuery.js');
 describe('query', function() {
 
   it('query keywords (q|search_query|query|etc) should be work', function() {
+    // match
     expect(extractQuery('https://tw.search.yahoo.com/search?ei=UTF-8&fr=crmas&p=pleasurazy')).to.equal('pleasurazy');
     expect(extractQuery('https://maps.googleapis.com/maps/api/?q=pleasurazy&page=2')).to.equal('pleasurazy');
     expect(extractQuery('https://maps.googleapis.com/maps/api/?search=pleasurazy&page=2')).to.equal('pleasurazy');
     expect(extractQuery('https://maps.googleapis.com/maps/api/?query=pleasurazy&page=2')).to.equal('pleasurazy');
     expect(extractQuery('https://maps.googleapis.com/maps/api/?search_query=pleasurazy&page=2')).to.equal('pleasurazy');
+
+    // do not match
+    expect(extractQuery('http://en.wikipedia.org/wiki/HTTP/2')).to.be.null;
+    expect(extractQuery('https://nt46.hackpad.com/ep/pad/static/cpaB7UzPlT8')).to.be.null;
+    expect(extractQuery('http://www.eztable.com/?utm_source=google_adwords&utm_campaign=brand_name_eztable&utm_medium=cpc&utm_p=a&utm_r=tw&gclid=Cj0KEQjwy7qrBRC4lp7_hM3dgIoBEiQA72pCnjuZqLTjcAad5HyNJL9ohcS0ctz5N7qvLtbxhnoHaqoaAlXv8P8HAQ')).to.be.null;
   });
 
   it('query in the last param using hash sign', function() {
